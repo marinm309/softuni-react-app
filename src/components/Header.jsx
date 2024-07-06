a5eb9b1dbe9643877c88fdad23e7ad161835c232
Marin Marinov <marinm309@abv.bg>
2024-07-06 17:17:36 +0300

    some work

---
+++ src/components/Header.jsx
@@ -0,0 +1,20 @@
+import { Link } from "react-router-dom"
+import styles from '../css-modules/Header.module.css'
+
+function Header(){
+    return(
+        <>
+            <ul className={styles['header-container']}>
+                <Link to={'/'}><li><img src="/logo-placeholder.jpg" className={styles['image']} /></li></Link>
+                <div className={styles['no-logo-container']}>
+                    <Link to={'/add-product'}><li>Add Product</li></Link>
+                    <Link to={'/login'}><li>Login</li></Link>
+                    <Link to={'/register'}><li>Register</li></Link>
+                    <Link to={'/profile'}><li><img src="/profile-placeholder.jpg" className={styles['image']} /></li></Link>
+                </div>
+            </ul>
+        </>
+    )
+}
+
+export default Header
